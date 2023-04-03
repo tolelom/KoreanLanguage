9ed5d525a6df5381723c128597edc29825365f45
tolelom <tolelom@naver.com>
2023-04-04 01:54:28 +0900

    test

---
+++ test/test.cpp
@@ -0,0 +1,5 @@
+//
+// Created by 김성민 on 2023/04/04.
+//
+
+#include "test.h"
---
+++ test/test.h
@@ -0,0 +1,14 @@
+//
+// Created by 김성민 on 2023/04/04.
+//
+
+#ifndef KOREANLANGUAGE_TEST_H
+#define KOREANLANGUAGE_TEST_H
+
+
+class test {
+
+};
+
+
+#endif //KOREANLANGUAGE_TEST_H
